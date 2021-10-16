aee33318966b4f206d402567d09aa3bcc12c75d0
tvey <tanyavoid@ya.ru>
2021-10-16 22:24:48 +0300

    Tries and tweaks

--- app/app.py
+++ app/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template, redirect, flash
+from flask import Flask, request, render_template, redirect, flash, jsonify
 
 from utils import get_playlist_id, get_result, SECRET_KEY
 
@@ -9,19 +9,23 @@ app.secret_key = SECRET_KEY
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
-    if request.method == 'POST':
-        playlist = request.form.get('playlist')
-        playlist_id = get_playlist_id(playlist)
-        if not playlist_id:
-            flash('Please add a valid playlist link or id.')
-            return redirect(request.url)
-        else:
-            result = get_result(playlist_id)
-            return render_template(
-                'index.html', result=result, playlist=playlist
-            )
-
+    # if request.method == 'POST':
+    #     playlist = request.form.get('playlist')
+    #     playlist_id = get_playlist_id(playlist)
+    #     if not playlist_id:
+    #         flash('Please add a valid playlist link or id.')
+    #         return redirect(request.url)
+    #     else:
+    #         result = get_result(playlist_id)
+    #         return render_template(
+    #             'index.html', result=result, playlist=playlist
+    #         )
     return render_template('index.html')
+
+
+@app.route('/result', methods=['GET'])
+def result():
+    return jsonify(123)
 
 
 if __name__ == '__main__':
--- app/static/script.js
+++ app/static/script.js
@@ -1,11 +1,22 @@
-let flashes = document.querySelectorAll('.flashes li')
+let playlistForm = document.getElementById('pl-form')
+let resultElem = document.getElementById('result')
 
-// flashes.forEach((item) => {
-//     item.classList.add('fade-out')
-// })
+let flashes = document.querySelectorAll('.flashes li')
+let spinner = document.querySelector('.spinner')
+
+
+playlistForm.addEventListener('submit', getResult)
+
+async function getResult() {
+  const response = await fetch('/result')
+  const result = await response.json()
+  resultElem.innerText = `Playlist duration: ${result}`
+}
 
 setTimeout(() => {
     flashes.forEach((item) => {
         item.remove()
     })
 }, 5000)
+
+spinner.style.display = 'none'
